6e4d8a4944fc3c9bdfb0146dd71cced4b24a3acc
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-03-16 15:56:53 +0100

    Terminado, pdte de hacer el bonus

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const cardTemplate = function (/* You can pass the data here*/) {
-  return `<div class="card">
-              <img id="flag-image" src="ADD THE IMAGE LINK HERE" alt="flag" />
-              <h1 class="center">ADD COUNTRY NAME HERE</h1>
+const cardTemplate = function (bandera, nombre) {
+  return  `<div class="card">
+              <img id="flag-image" src="${bandera}" alt="flag" />
+              <h1 class="center">${nombre}</h1>
             </div>`;
 };
 
-const countriesNode = document.getElementById("countries");
+  const countriesNode = document.getElementById("countries");
 
-fetch(/* Need the provide API URL to get all countries */)
-  .then(function (response) {
-    // fetch() returns a promise containing the response (a Response object).
-    // This is just an HTTP response, not the actual JSON. 
-    // To extract the JSON body content from the response, 
-    // we use the json() method and pass it into the next .then()
-  })
+fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
   .then(function (countries) {
+    const newCountry = countries.map(e =>{
+      let bandera = e.flags.png
+      let nombre = e.name.common
+      countriesNode.innerHTML += cardTemplate(bandera, nombre)
+    })
     // Here is where you'll need to add into the DOM all the countries received from API 
 
     // 1 - We will need to iterate the countries variable with a loop
